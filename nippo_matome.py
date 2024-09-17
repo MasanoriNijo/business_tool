844d9bc0ee13f1695958407039b05b69452b53ef
二條 正則 <masanori.nijo@ad.s-cubism.jp>
2024-09-17 22:28:40 +0900

    update

--- nippo_matome.py
+++ nippo_matome.py
@@ -4,6 +4,8 @@ import email
 from email.header import decode_header
 import re
 from datetime import datetime, timedelta
+import sys
+import copy
 
 # メールサーバに接続
 def connect_to_email_server(username, password, imap_server="imap.gmail.com"):
@@ -24,19 +26,20 @@ def connect_to_email_server(username, password, imap_server="imap.gmail.com"):
 
 
 # メールの件名を正規表現でフィルタリング
-def filter_emails_by_subject(mail, folder="inbox", keyword_regex=".*"):
+def filter_emails_by_subject(mail, n_days_ago, folder="inbox", keyword_regex=".*"):
     mail.select(folder)
     
     # メール一覧を取得(全件)
     # result, data = mail.search(None, "ALL")
     
     # 直近N日分のメールを検索
-    N = 7  # 例として直近7日間
+    N = int(n_days_ago)  
     date_N_days_ago = (datetime.now() - timedelta(days=N)).strftime('%d-%b-%Y')
-
+    print(date_N_days_ago)
     # SINCEコマンドでN日前以降のメールを検索
     result, data = mail.search(None, f'SINCE {date_N_days_ago}')
     filtered_emails = []
+    subjects = []
     if result == "OK":
         email_ids = data[0].split()
         for email_id in email_ids:
@@ -53,8 +56,9 @@ def filter_emails_by_subject(mail, folder="inbox", keyword_regex=".*"):
                     # 正規表現で件名フィルタリング
                     if re.search(keyword_regex, subject):
                         filtered_emails.append(msg)
+                        subjects.append(subject)
     
-    return filtered_emails
+    return [filtered_emails, subjects]
 
 # メールの内容をテキストとして抽出
 def extract_text_from_email(msg):
@@ -77,24 +81,27 @@ def extract_text_from_email(msg):
     return ""
 
 # テキストの内容を成型してデータ構造として保持する。
-def _buf_add_to_nippo(buf, nippo):
-    # 追加する。       
-    while len(buf) < 3:
-        buf.append("N")  
-    if buf[0] not in nippo:
-        nippo[buf[0]] = {}
-    if buf[1] not in nippo[buf[0]]:
-        nippo[buf[0]][buf[1]] = []
-    if buf[2] not in nippo[buf[0]][buf[1]]:
-        nippo[buf[0]][buf[1]].append(buf[2])
+def _buf_add_to_nippo(buf, nippo, date = ""):
+    # 追加する。
+    buf_ = copy.deepcopy(buf)      
+    while len(buf_) < 3:
+        buf_.append("N")  
+    if buf_[0] not in nippo:
+        nippo[buf_[0]] = {}
+    if buf_[1] not in nippo[buf_[0]]:
+        nippo[buf_[0]][buf_[1]] = []
+    if buf_[2] not in nippo[buf_[0]][buf_[1]]:
+        nippo[buf_[0]][buf_[1]].append(buf_[2] + date)
 
-def exchange_text_to_nippo(mailTxt, nippo = {}):
+def exchange_text_to_nippo(mailTxt, nippo = {}, date = ""):
     mailTxts = []
     for item in mailTxt.split("\r\n"):
         item = item.strip()
+        item = item.strip("\n")
+        item = item.strip("\r")
         if item:
             mailTxts.append(item)
-    finRegex = "\d{4}年\d{1,2}月\d{1,2}日\([日月火水木金土]\) \d{2}:\d{2}" # 2024年9月12日(木) 10:43 二條正則 <masanori.nijo@s-cubism.jp>: のところで終わりにする。
+    finRegex = "\d{4}年\d{1,2}月\d{1,2}日\([日月火水木金土]\) \d{1,2}:\d{1,2}" # 2024年9月12日(木) 10:43 二條正則 <masanori.nijo@s-cubism.jp>: のところで終わりにする。
     komokuRegexs = ["▼","・","→",finRegex]
     
     startFlg = False # 記載対象が始まったらTrue
@@ -102,7 +109,8 @@ def exchange_text_to_nippo(mailTxt, nippo = {}):
     buf = [] # 一時的に保持
     for mltxt in mailTxts:
         if startFlg:
-            matchedFlg = False        
+            matchedFlg = False
+            append_text_to_file(mltxt, "nippou_all.txt")        
             for ind_ in range(4):
                 match = re.match("^" + komokuRegexs[ind_] + "(.*)",mltxt)
                 if match:
@@ -112,13 +120,13 @@ def exchange_text_to_nippo(mailTxt, nippo = {}):
                         ind += 1
                     elif ind == ind_:
                         # 追加する。       
-                        _buf_add_to_nippo(buf, nippo)
+                        _buf_add_to_nippo(buf, nippo, date)
                         # bufを戻す。
                         buf.pop()
                         buf.append(match[1])
                     elif ind_ < 3:
                         # 追加する。       
-                        _buf_add_to_nippo(buf, nippo)
+                        _buf_add_to_nippo(buf, nippo, date)
                         # bufを戻す。
                         while ind >= ind_:
                             buf.pop()
@@ -127,18 +135,17 @@ def exchange_text_to_nippo(mailTxt, nippo = {}):
                         ind += 1
                     else: # 追記対象の最後を感知した場合。
                         # 最後を追加する。そして処理を抜ける。
-                        _buf_add_to_nippo(buf, nippo) 
+                        _buf_add_to_nippo(buf, nippo, date) 
                         return nippo
                     break
             if not matchedFlg:
                 # この場合は改行に内容が書かれている場合なので、追記する。
                 buf[-1] += mltxt
         else: # 処理対象の始まりを判定する
-            match = re.match("^" + komokuRegexs[0] + "(.*)",mltxt)
+            match = re.match("^" + komokuRegexs[0] + "(.*)", mltxt)
             if match:
                 startFlg = True
                 buf.append(match[1])
-                nippo[match[1]]={}
                 ind = 0
     return nippo
                 
@@ -148,7 +155,7 @@ def append_text_to_file(text, filename):
         # 'a' モードでファイルを開く（ファイルがなければ作成される）
         with open(filename, 'a') as file:
             file.write(text + '\n')  # 指定されたテキストを追記
-        print(f"'{text}' has been appended to {filename}.")
+        # print(f"'{text}' has been appended to {filename}.")
     except IOError as e:
         # 入出力エラーをキャッチ
         print(f"An I/O error occurred: {e}")
@@ -182,21 +189,32 @@ def ai_summarize_texts(texts, max_length=150):
     return summary[0]['summary_text']
 
 # メイン処理
-def main(username, password, keyword_regex):
+def main(username, password, keyword_regex, n_days_ago = 7):
     # 1. メールサーバーに接続
     mail = connect_to_email_server(username, password)
     
     # 2. 件名でフィルタリング
     # filtered_emails = filter_emails_by_subject(mail, folder = "'[Gmail]/Sent Mail'", keyword_regex=keyword_regex)
-    filtered_emails = filter_emails_by_subject(mail, folder = "[Gmail]/&kAFP4W4IMH8w4TD8MOs-", keyword_regex=keyword_regex)
+    filtered_emails = filter_emails_by_subject(mail, n_days_ago, folder = "[Gmail]/&kAFP4W4IMH8w4TD8MOs-", keyword_regex = keyword_regex)
     
     # 3. フィルタされたメールの本文を抽出
-    extracted_texts = [extract_text_from_email(email) for email in filtered_emails]
-    
+    extracted_texts = [extract_text_from_email(email) for email in filtered_emails[0]]
+    subjects = filtered_emails[1]
+    print(subjects)
     nippo = {}
-    for extracted_text in extracted_texts:
+    for ind, extracted_text in enumerate(extracted_texts):
+        dateRegex = ".*\d{4}/(\d{1,2}/\d{1,2}).*"
+        date = ""
+        append_text_to_file(subjects[ind], "nippou_all.txt")
+        match = re.match(dateRegex, subjects[ind])
+        if match:
+            date = " (" + match[1] + ")"
         # append_text_to_file(extracted_text, "nippou.txt")
-        nippo = exchange_text_to_nippo(extracted_text, nippo)
+        nippo = exchange_text_to_nippo(extracted_text, nippo, date)
+        nippoTxt = exportDataRecursive(nippo)
+        subjects[ind]
+        append_text_to_file(subjects[ind], "nippou_xxx.txt")
+        append_text_to_file(nippoTxt, "nippou_xxx.txt")
         
     nippoTxt = exportDataRecursive(nippo)
     append_text_to_file(nippoTxt, "nippou.txt")
@@ -214,5 +232,14 @@ if __name__ == "__main__":
     # 件名のフィルタリングキーワード（正規表現）
     keyword_regex = "^Re: 【勤怠連絡】.*"
     
+    n_days_ago = 31
+    if len(sys.argv) > 1:
+        # 最初の要素はスクリプト名なので、それ以降を取得
+        n_days_ago = sys.argv[1]
+
     # 実行
-    main(username, password, keyword_regex)
+    main(username, password, keyword_regex, n_days_ago)
+    
+# 使い方
+# python3 nippo_matome.py N (Nは過去何日分 省略した場合7 直近一週間分)
+ 
